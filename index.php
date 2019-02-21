535261f72fd2ddab5f4ce1293e6668403962149d
topman99 <topman99@gmail.com>
2019-02-21 14:07:24 +0800

    Update index.php

--- index.php
+++ index.php
@@ -29,8 +29,8 @@
   			echo date("Y m d");
   		?> 
   		<p>
-  			<a href="/login.php">Login</a>
-  			<a href="/register.php">Register</a>
+  			<a href="/php_login_system/login.php">Login</a>
+  			<a href="/php_login_system/register.php">Register</a>
   		</p>
   	</div>
 
